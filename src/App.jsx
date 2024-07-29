91c9c62bb8722d85906011bbd72ac44fc0e94d3e
AbdulHaseb9 <haseebsheikh2380@gmail.com>
2024-07-29 14:46:29 -0700

    initialize project add sass

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './style/app.scss'
+
+function App() {
+
+  return (
+    <>
+      <div className='demo'>
+        <h1>Quiz App</h1>
+        <h2>Using React + Sass</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
